e0427390531fc11ae2561c9d723fdaeceeb66096
Vitaliano <vitaliano.neto@gmail.com>
2016-01-31 19:07:44 -0200

    System predicting when the path will be blocked

---
+++ Atom Defender/Assets/Scripts/Creeper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creeper : MonoBehaviour {
+    public float speed = 1;
+
+    private Seeker pathFinder;
+
+	void Start () {
+        pathFinder = GetComponent<Seeker>();
+	}
+	
+	void Update ()
+    {
+        var path = pathFinder.GetCurrentPath();
+        //Debug.Log(pathFinder.GetCurrentPath() == null);
+        if (path != null)
+        {
+            //path.ge
+        }
+	}
+}
--- Atom Defender/Assets/Scripts/SceneManager.cs
+++ Atom Defender/Assets/Scripts/SceneManager.cs
@@ -1,34 +1,47 @@
 ﻿using UnityEngine;
 using Pathfinding;
+using System.Collections.Generic;
 
 public class SceneManager : MonoBehaviour {
-    public GameObject turret1Base;
-    public GameObject turret2Base;
-    public GameObject creeper1Base;
-    public GameObject creeper2Base;
+    public GameObject turret1Prefab;
+    public GameObject turret2Prefab;
+    public GameObject creeper1Prefab;
+    public GameObject creeper2Prefab;
     public GameObject positionSelector;
     public Vector3 creeperStartPosition;
     public Vector3 creeperTargetPosition;
 
     private Seeker pathTester;
     private Material positionSelectorMaterial;
+    private GraphUpdateScene positionSelectorGraph;
+    private ABPath defaultPath;
+    private Color positionSelectorInitialColor;
+    private Int3 lastValidNodePosition;
+    private List<GraphNode> lastNodesChanged = new List<GraphNode>();
+    private GraphNode creeperStartNode;
+    private GraphNode creeperTargetNode;
 
-	void Start ()
+    void Start ()
     {
         pathTester = GetComponent<Seeker>();
-        if(positionSelector)
+        defaultPath = (ABPath)pathTester.StartPath(creeperStartPosition, creeperTargetPosition);//pathTester.GetNewPath(creeperStartPosition, creeperTargetPosition);
+        if (positionSelector)
         {
             var r = positionSelector.GetComponent<MeshRenderer>();
             positionSelectorMaterial = r.material;
+            positionSelectorInitialColor = r.sharedMaterial.color;
+            positionSelectorGraph =  positionSelector.GetComponent<GraphUpdateScene>();
         }
-	}
+        creeperStartNode = AstarPath.active.GetNearest(creeperStartPosition).node;
+        creeperTargetNode = AstarPath.active.GetNearest(creeperTargetPosition).node;
+    }
 	
     void Update()
     {
         if(positionSelectorMaterial)
         {
             var c = positionSelectorMaterial.color;
-            c.a = (Mathf.Sin(Time.time * 2) + 1.1f) * 0.15f;
+            c.a = (Mathf.Sin(Time.time * 2) + 1.3f) * 0.1f;
             positionSelectorMaterial.color = c;
         }
     }
@@ -43,6 +56,68 @@ public class SceneManager : MonoBehaviour {
             {
                 var node = AstarPath.active.GetNearest(hitInfo.point).node;
                 positionSelector.transform.position = (Vector3)node.position;
+                var connectionCount = 0;
+                node.GetConnections(delegate (GraphNode other) { connectionCount++; });
+                if(connectionCount != 8)
+                {
+                    var c = Color.red;
+                    c.a = positionSelectorMaterial.color.a;
+                    positionSelectorMaterial.color = c;
+                    //if (lastNodesChanged.Count > 0)
+                    //{
+                    //    foreach (var n in lastNodesChanged)
+                    //    {
+                    //        n.Tag = 1;
+                    //    }
+                    //    lastNodesChanged.Clear();
+                    //}
+                }
+                else
+                {
+                    bool validPath = false;
+                    if (lastValidNodePosition != node.position)
+                    {
+                        //if(lastNodesChanged.Count > 0)
+                        //{
+                        //    foreach(var n in lastNodesChanged)
+                        //    {
+                        //        n.Tag = 1;
+                        //    }
+                        //    lastNodesChanged.Clear();
+                        //}
+                        //positionSelector.GetComponent<GraphUpdateScene>().Apply();
+                        //lastNodesChanged.AddRange((AstarPath.active.graphs[0] as GridGraph).GetNodesInArea(positionSelectorGraph.GetBounds()));
+                        //foreach (var n in lastNodesChanged)
+                        //{
+                        //    n.Tag = 2;
+                        //}
+                        //defaultPath = (ABPath)pathTester.StartPath(creeperStartPosition, creeperTargetPosition);
+                        //AstarPath.WaitForPath(defaultPath);
+                        lastValidNodePosition = node.position;
+                    }
+                    //var distance = Vector3.Distance(defaultPath.endPoint, creeperTargetPosition);
+                    //if (distance < 0.5f)
+                    var guo = new GraphUpdateObject(positionSelectorGraph.GetBounds());
+                    guo.modifyWalkability = positionSelectorGraph.modifyWalkability;
+                    guo.setWalkability = positionSelectorGraph.setWalkability;
+                    Debug.Log(positionSelectorGraph.GetBounds());
+                    if (GraphUpdateUtilities.UpdateGraphsNoBlock(guo, creeperStartNode, creeperTargetNode, true))
+                    {
+                        validPath = true;
+                    }
+                    if (validPath)
+                    {
+                        var c = positionSelectorInitialColor;
+                        c.a = positionSelectorMaterial.color.a;
+                        positionSelectorMaterial.color = c;
+                    }
+                    else
+                    {
+                        var c = Color.red;
+                        c.a = positionSelectorMaterial.color.a;
+                        positionSelectorMaterial.color = c;
+                    }
+                }
             }
         }
     }
