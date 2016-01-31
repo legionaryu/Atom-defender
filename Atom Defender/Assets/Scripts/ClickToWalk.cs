e5b435c9c0de367cf3f01023162871c745b49a9b
Vitaliano <vitaliano.neto@gmail.com>
2016-01-31 09:48:11 -0200

    Imported the AstarPathfinding from Aron Granberg

---
+++ Atom Defender/Assets/Scripts/ClickToWalk.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Pathfinding;
+using System.Linq;
+
+public class ClickToWalk : MonoBehaviour
+{
+    private Seeker agent;
+	void Start()
+    {
+        agent = GetComponent<Seeker>();
+    }
+
+	void Update ()
+    {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(ray, out hitInfo))
+            {
+                //Debug.Log("CalculatePath:" + agent.CalculatePath(hitInfo.point, path));
+                //agent.GetNewPath(agent.transform.position, hitInfo.point).
+                Debug.Log("SetDestination:" + agent.StartPath(agent.transform.position, hitInfo.point, OnPathComplete) + " | hitInfo.point:" + hitInfo.point);
+                //Debug.Log("SetDestination:" + agent.GetNewPath(agent.transform.position, hitInfo.point) + " | hitInfo.point:" + hitInfo.point);
+            }
+        }
+	}
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("Len:" + p.path.Count + " | " + string.Join(",", p.path.Select(x => x.position.ToString()).ToArray()));
+    }
+
+}
---
+++ Atom Defender/Assets/Scripts/DynamicObstacle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using Pathfinding;
+
+public class DynamicObstacle : MonoBehaviour {
+	void Start () {
+        var obstacle = GetComponent<GraphUpdateScene>();
+        obstacle.Apply();
+        //AstarPath.active.UpdateGraphs(obstacle as GraphUpdateObject);
+    }
+}
