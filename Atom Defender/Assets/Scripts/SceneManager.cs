a525a8f95cfb06c9d12152204edab97d6d3e3eb1
Vitaliano <vitaliano.neto@gmail.com>
2016-01-31 11:22:25 -0200

    Set the basis for the gameplay development

---
+++ Atom Defender/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+    public GameObject turret1Base;
+    public GameObject turret2Base;
+    public GameObject creeper1Base;
+    public GameObject creeper2Base;
+    public Vector3 creeperStartPosition;
+    public Vector3 creeperTargetPosition;
+
+    private Seeker pathTester;
+
+	void Start () {
+        pathTester = GetComponent<Seeker>();
+	}
+	
+	void Update () {
+	
+	}
+}
